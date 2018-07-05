b01ac072418fc4a8bcf88e94906b3bd94d33d259
Jeff Baker <sinerasis@gmail.com>
2018-07-05 19:57:49 +0000

    add write to file functionality

--- cron.uptime.sh
+++ cron.uptime.sh
@@ -13,6 +13,9 @@ for i in ${sites[@]}; do
 	# uses curl to make http request and save the status code to a variable
 	status=$(curl -o /dev/null -u root:root -Isw '%{http_code}' ${i})
 
+	# write result to file
+	echo `date`","${i}","${status} >> /var/log/cron.uptime.log
+
 	# check the status code to see if it's 200, send an email if not
 	if [ ${status} -ne "200" ]; then
 		# full path of binary must be used because cron has a limited PATH!
